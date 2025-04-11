3384e71c45177ef56b8ce267d85107bcaf8f4c8a
bak0ni <bakoni@ymail.com>
2025-04-11 19:57:03 +0100

    Update variables.tf
    ip addressing

--- terraform/landing-zone/variables.tf
+++ terraform/landing-zone/variables.tf
@@ -6,12 +6,12 @@ variable "random_string" {
 
 variable "frc-pyt-vnet-cidr" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.1.0.0/16"
 }
 
 variable "frc-dotnet-vnet-cidr" {
   type    = string
-  default = "10.1.0.0/16"
+  default = "10.2.0.0/16"
 }
 
 variable "tags" {
@@ -25,10 +25,5 @@ variable "tags" {
 variable "vwan-region1-hub1-prefix1" {
   type    = string
   default = "10.0.0.0/16"
-}
-
-variable "vwan-region2-hub1-prefix1" {
-  type    = string
-  default = "10.10.0.0/16"
 }
 
