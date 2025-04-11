08c0f8e096d52d4e4c0e33497a7d87d47e048b8b
bak0ni <bakoni@ymail.com>
2025-04-11 20:11:16 +0100

    Update variables.tf
    Added vnet cidr

--- terraform/landing-zone/variables.tf
+++ terraform/landing-zone/variables.tf
@@ -14,11 +14,62 @@ variable "frc-dotnet-vnet-cidr" {
   default = "10.2.0.0/16"
 }
 
+variable "frc-game-vnet-cidr" {
+  type    = string
+  default = "10.3.0.0/16"
+}
+
+variable "frc-soec-vnet-cidr" {
+  type    = string
+  default = "10.4.0.0/16"
+}
+
+variable "itn-pyt-vnet-cidr" {
+  type    = string
+  default = "10.5.0.0/16"
+}
+
+variable "itn-dotnet-vnet-cidr" {
+  type    = string
+  default = "10.6.0.0/16"
+}
+
+variable "itn-game-vnet-cidr" {
+  type    = string
+  default = "10.7.0.0/16"
+}
+
+variable "itn-spec-vnet-cidr" {
+  type    = string
+  default = "10.8.0.0/16"
+}
+
+variable "uks-pyt-vnet-cidr" {
+  type    = string
+  default = "10.9.0.0/16"
+}
+
+variable "uks-dotnet-vnet-cidr" {
+  type    = string
+  default = "10.10.0.0/16"
+}
+
+variable "uks-game-vnet-cidr" {
+  type    = string
+  default = "10.11.0.0/16"
+}
+
+variable "uks-spec-vnet-cidr" {
+  type    = string
+  default = "10.12.0.0/16"
+}
+
 variable "tags" {
   description = "Default tags to apply to all resources."
   type        = map(any)
   default = {
     env      = "Development"
+    proj     = "Zero Trust"
   }
 }
 
